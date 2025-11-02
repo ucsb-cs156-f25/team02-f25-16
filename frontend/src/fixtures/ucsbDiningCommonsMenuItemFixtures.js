9c9aac7339b2d5596eaa57535ed2084edf543104
Alex Jeong <alexjhjeong@gmail.com>
2025-11-01 17:37:56 -0700

    aj - WIP fix formatting

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,30 +1,30 @@
 const ucsbDiningCommonsMenuItemFixtures = {
-    oneMenuItem: {
-        "id": 1,
-        "diningCommonsCode": "chicken",
-        "name": "Chicken",
-        "station": "Protein"
+  oneMenuItem: {
+    id: 1,
+    diningCommonsCode: "chicken",
+    name: "Chicken",
+    station: "Protein",
+  },
+  threeMenuItems: [
+    {
+      id: 1,
+      diningCommonsCode: "chicken",
+      name: "Chicken",
+      station: "Protein",
     },
-    threeMenuItems: [
-        {
-            "id": 1,
-            "diningCommonsCode": "chicken",
-            "name": "Chicken",
-            "station": "Protein"
-        },
-        {
-            "id": 2,
-            "diningCommonsCode": "chocolate",
-            "name": "Chocolate",
-                "station": "Dessert"
-        },
-        {
-            "id": 3,
-            "diningCommonsCode": "pizza",
-            "name": "Pizza",
-            "station": "Entree"
-        },
-    ]
+    {
+      id: 2,
+      diningCommonsCode: "chocolate",
+      name: "Chocolate",
+      station: "Dessert",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "pizza",
+      name: "Pizza",
+      station: "Entree",
+    },
+  ],
 };
 
-export {ucsbDiningCommonsMenuItemFixtures}
+export { ucsbDiningCommonsMenuItemFixtures };
