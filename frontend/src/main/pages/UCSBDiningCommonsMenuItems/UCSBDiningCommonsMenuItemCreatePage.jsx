6b63f87b027b7ce9cf7727cdd3acbdccdc1b5ffe
Alex Jeong <alexjhjeong@gmail.com>
2025-11-03 22:27:53 -0800

    aj - UCSBDiningCommonsMenuItemCreatePage and tests

--- frontend/src/main/pages/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemCreatePage.jsx
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemCreatePage.jsx
@@ -4,7 +4,9 @@ import { Navigate } from "react-router";
 import { useBackendMutation } from "main/utils/useBackend";
 import { toast } from "react-toastify";
 
-export default function UCSBDiningCommonsMenuItemCreatePage({ storybook = false }) {
+export default function UCSBDiningCommonsMenuItemCreatePage({
+  storybook = false,
+}) {
   const objectToAxiosParams = (UCSBDiningCommonsMenuItem) => ({
     url: "/api/ucsbdiningcommonsmenuitems/post",
     method: "POST",
