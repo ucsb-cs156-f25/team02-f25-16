ef14ec85929076d15da4c1a4d8bc733ba81be05d
mihir-s-05 <107030526+mihir-s-05@users.noreply.github.com>
2025-11-05 02:35:45 -0500

    Revert "Revert "Aj ucsb dining commons menu item index page""

--- frontend/src/tests/utils/UCSBDiningCommonsMenuItemUtils.test.js
+++ frontend/src/tests/utils/UCSBDiningCommonsMenuItemUtils.test.js
@@ -41,7 +41,7 @@ describe("UCSBDiningCommonsMenuItemUtils", () => {
 
       // assert
       expect(result).toEqual({
-        url: "/api/UCSBDiningCommonsMenuItems",
+        url: "/api/ucsbdiningcommonsmenuitems",
         method: "DELETE",
         params: { id: 17 },
       });
