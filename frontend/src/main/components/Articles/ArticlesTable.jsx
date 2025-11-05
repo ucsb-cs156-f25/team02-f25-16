65ecbba49e461cffb040f6fbea6ea5f120333180
Mihir Srivastava <m_srivastava@ucsb.edu>
2025-11-04 21:56:29 -0800

    implemented index page functionality and tests

--- frontend/src/main/components/Articles/ArticlesTable.jsx
+++ frontend/src/main/components/Articles/ArticlesTable.jsx
@@ -1,10 +1,37 @@
 import React from "react";
-import OurTable from "main/components/OurTable";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import {
+  cellToAxiosParamsDelete,
+  onDeleteSuccess,
+} from "main/utils/articlesUtils";
+import { useNavigate } from "react-router";
+import { hasRole } from "main/utils/useCurrentUser";
 
 export default function ArticlesTable({
   articles,
+  currentUser,
   testIdPrefix = "ArticlesTable",
 }) {
+  const navigate = useNavigate();
+
+  const editCallback = (cell) => {
+    navigate(`/articles/edit/${cell.row.original.id}`);
+  };
+
+  // Stryker disable all : hard to test for query caching side effects
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
+    ["/api/articles/all"],
+  );
+  // Stryker restore all
+
+  const deleteCallback = async (cell) => {
+    deleteMutation.mutate(cell);
+  };
+
   const columns = [
     {
       header: "ID",
@@ -31,6 +58,13 @@ export default function ArticlesTable({
       accessorKey: "dateAdded",
     },
   ];
+
+  if (hasRole(currentUser, "ROLE_ADMIN")) {
+    columns.push(ButtonColumn("Edit", "primary", editCallback, testIdPrefix));
+    columns.push(
+      ButtonColumn("Delete", "danger", deleteCallback, testIdPrefix),
+    );
+  }
 
   return <OurTable data={articles} columns={columns} testid={testIdPrefix} />;
 }
